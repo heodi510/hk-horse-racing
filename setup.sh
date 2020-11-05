2690d80cd9d2161b97429c5807a1edc4eaffa629
heodi510 <wongsiuhei510@gmail.com>
2020-11-05 15:06:19 +0800

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 mkdir -p ~/.streamlit/
-echo “\
-[general]\n\
-email = \”wongsiuhei510@gmail.com\”\n\
-“ > ~/.streamlit/credentials.toml
-echo “\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = $PORT\n\
-“ > ~/.streamlit/config.toml
+echo "
+[general]
+email = 'wongsiuhei510@gmail.com'
+" > ~/.streamlit/credentials.toml
+
+echo "[server]
+headless = true
+port = $PORT
+enableCORS = false
+" > ~/.streamlit/config.toml
